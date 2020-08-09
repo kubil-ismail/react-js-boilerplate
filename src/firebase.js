f6ff7304f228b6f13826a3f908ad66d0892e2a67
Kubil Ismail <bilkisismail07@gmail.com>
2020-08-10 02:19:53 +0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,36 @@
+import firebase from 'firebase/app';
+
+// Import services
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Import dotenv
+const {
+  REACT_APP_FIREBASE,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROKJECT_ID,
+  REACT_APP_STORAGE,
+  REACT_APP_MSG_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASUREMENT_ID
+} = process.env;
+
+// Config firebase
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROKJECT_ID,
+  storageBucket: REACT_APP_STORAGE,
+  messagingSenderId: REACT_APP_MSG_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENT_ID,
+};
+
+// Firebase init app
+firebase.initializeApp(firebaseConfig);
+
+// Export services
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,15 +4,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { login } from '../redux/actions/authActions';
 
+// import firebase from 'firebase/app';
+import firebase from 'firebase/app';
+import { googleProvider } from '../firebase';
+
 export class Profile extends Component {
   componentDidMount = () => {
     this.props.login();
   }
 
+  googleAuth = () => {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+  };
+
   render() {
     return (
       <>
         <p>Profile App</p>  
+        <button onClick={() => this.googleAuth()}>Login</button>
       </>
     );
   }
